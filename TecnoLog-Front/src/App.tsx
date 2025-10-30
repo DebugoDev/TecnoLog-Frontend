56c1d806592b52bfddbd84a576feb5b7dbe31d0e
loregbrw <loregobara@gmail.com>
2025-10-29 21:46:36 -0300

    feat: toastfy

--- TecnoLog-Front/src/App.tsx
+++ TecnoLog-Front/src/App.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import AppRoutes from "./routes/AppRoutes";
+import { ToastContainer } from "react-toastify";
 
 
 const App: React.FC = () => {
-  return (
-    <>
-      <AppRoutes />
-    </>
-  );
+    return (
+        <>
+            <AppRoutes />
+            <ToastContainer
+                position="bottom-right" />
+        </>
+    );
 };
 
 export default App;
--- TecnoLog-Front/src/layouts/MainLayout.tsx
+++ TecnoLog-Front/src/layouts/MainLayout.tsx
@@ -3,24 +3,24 @@ import Sidebar from "../components/Sidebar";
 import Header from "../components/Header";
 
 interface MainLayoutProps {
-  children: React.ReactNode;
-  title: string;
-  userName: string;
-  userEmail: string;
+    children: React.ReactNode;
+    title: string;
+    userName: string;
+    userEmail: string;
 }
 
 const MainLayout: React.FC<MainLayoutProps> = ({ children, title, userName, userEmail }) => {
-  return (
-    <div className="relative h-screen">
-      <Sidebar />
-      <div className="ml-16 flex flex-col h-full transition-all duration-300">
-        <Header title={title} userName={userName} userEmail={userEmail} />
-        <main className="flex-1 p-6 overflow-y-auto bg-[#ededed]">
-          {children}
-        </main>
-      </div>
-    </div>
-  );
+    return (
+        <div className="relative h-screen">
+            <Sidebar />
+            <div className="ml-16 flex flex-col h-full transition-all duration-300">
+                <Header title={title} userName={userName} userEmail={userEmail} />
+                <main className="flex-1 p-6 overflow-y-auto bg-[#ededed]">
+                    {children}
+                </main>
+            </div>
+        </div>
+    );
 };
 
 export default MainLayout;
